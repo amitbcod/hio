ead7c49f640dc70dec24c1b685cdb747c91c782b
mehul-bcod <mehul@bcod.co.in>
2026-06-25 11:45:03 +0530

    invoice changes

--- app/Http/Controllers/Frontend/TripController.php
+++ app/Http/Controllers/Frontend/TripController.php
@@ -1125,15 +1125,15 @@ body { font-family:helvetica; color:#222; font-size:10px; line-height:1.4; }
       <td align="center">&nbsp;</td>
     </tr>
     <tr>
-      <td><table width="100%" border="0" cellpadding="2" cellspacing="0" class="service-table">
+      <td><table width="100%" border="0" cellpadding="2" cellspacing="0" class="">
     <thead>
     <tr>
-        <th style="width:25%;">SERVICE</th>
-        <th style="width:25%;">SERVICE DATES</th>
-        <th style="width:25%;">DESCRIPTION</th>
-        <th style="width:5%;text-align:center;">QTY</th>
-        <th style="width:5%;text-align:center;">UNIT</th>
-        <th style="width:5%;text-align:center;">TOTAL</th>
+        <th style="width:22%;">SERVICE</th>
+        <th style="width:18%;">SERVICE DATES</th>
+        <th style="width:20%;">DESCRIPTION</th>
+        <th style="width:8%;text-align:center;">QTY</th>
+        <th style="width:10%;text-align:center;">UNIT</th>
+        <th style="width:8%;text-align:center;">TOTAL</th>
     </tr>
     </thead>
     <tbody>
