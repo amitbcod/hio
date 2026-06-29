0ac5c711999edffce7f7521bdc07c7d74dbd2e58
Amit Patel <amitp@bcod.co.in>
2026-06-29 16:53:48 +0530

    change in checkout page

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -663,18 +663,18 @@
                                     $rooms  = (int) ($item['rooms'] ?? 1);
                                     $lbl = $item['type'] === 'accommodation'
                                         ? $rooms . ' Room' . ($rooms !== 1 ? 's' : '') . ' · ' . $nights . ' Night' . ($nights !== 1 ? 's' : '')
-                                        : 'Activity: ' . ($item['variant_name'] ?: $item['title']);
+                                        : ' ' . ($item['variant_name'] ?: $item['title']);
                                 @endphp
                                 <div class="fare-row">
                                     <span>{{ $lbl }}</span>
-                                    <span>{{ $item['currency'] }} {{ number_format($item['total_price'], 2) }}</span>
+                                    <span> {{ number_format($item['total_price'], 2) }}</span>
                                 </div>
                             @endforeach
 
                             @if($summary['total_discount'] > 0)
                                 <div class="fare-row fare-row--discount">
                                     <span><i class="fa-solid fa-tag"></i> {{ __('cart.discounts') }}</span>
-                                    <span>−{{ $summary['currency'] }} {{ number_format($summary['total_discount'], 2) }}</span>
+                                    <span>− {{ number_format($summary['total_discount'], 2) }}</span>
                                 </div>
                                 <div class="fare-row">
                                     <span>{{ __('cart.price_after_discount') }}</span>
@@ -683,7 +683,7 @@
                             @if($summary['total_tax'] > 0)
                                 <div class="fare-row">
                                     <span>{{ __('checkout.taxes_charges') }}</span>
-                                    <span>{{ $summary['currency'] }} {{ number_format($summary['total_tax'], 2) }}</span>
+                                    <span> {{ number_format($summary['total_tax'], 2) }}</span>
                                 </div>
                             @endif
 
