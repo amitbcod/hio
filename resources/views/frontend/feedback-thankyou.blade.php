2f932058ede90bdae05d6e4e0b5fa6c904a07141
mehul-bcod <mehul@bcod.co.in>
2026-06-29 15:15:12 +0530

    feedback style

--- resources/views/frontend/feedback-thankyou.blade.php
+++ resources/views/frontend/feedback-thankyou.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+.thankyou-box {
+    max-width:400px;
+    margin:0 auto;
+    width:100%;
+    text-align: center;
+}
+</style>
 <div class="container">
   <div class="thankyou-box">
     <h1>Thank you</h1>
     <p>{{ __('feedback.thankyou', ['id' => $trip->id]) }}</p>
-    
   </div>
 </div>
 @endsection
