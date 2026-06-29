13199349b1407ff58f42d6c4477892aa641e9b4a
mehul-bcod <mehul@bcod.co.in>
2026-06-29 15:30:04 +0530

    feedback style

--- resources/views/frontend/feedback-thankyou.blade.php
+++ resources/views/frontend/feedback-thankyou.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
 <style>
-.thankyou-box {
+  .thank-page {
     max-width: 470px;
     margin: 60px auto;
     width: 100%;
     text-align: center;
-    background: var(--card);
+  }
+.thankyou-box {
+    width:100%;
+    text-align: center;
+    background: #ffffff;
     border: 1px solid #e5dfd6;
     border-radius: 12px;
     box-shadow: 0 12px 32px rgba(15, 23, 42, 0.06);
@@ -15,11 +19,12 @@
 }
 </style>
 <div class="container">
-  <div class="thankyou-box">
-    <h1>Thank you</h1>
-    <p>{{ __('feedback.thankyou', ['id' => $trip->id]) }}</p>
+  <div class="thank-page">
+    <div class="thankyou-box">
+      <h1>Thank you</h1>
+      <p>{{ __('feedback.thankyou', ['id' => $trip->id]) }}</p>
+    </div>
+    <a href="{{ url('/') }}" class="btn-home">Go to Home</a>
   </div>
-
-  <a href="{{ url('/') }}" class="btn-home">Go to Home</a>
 </div>
 @endsection
