85f8c8b98f6ff8043854319e1ab210ada78c915c
mehul-bcod <mehul@bcod.co.in>
2026-07-01 15:47:35 +0530

    pdf changes

--- app/Http/Controllers/Frontend/TripController.php
+++ app/Http/Controllers/Frontend/TripController.php
@@ -1270,16 +1270,7 @@ $mealPlanSafe = !empty($mealPlanValues)
 <table width="100%" cellpadding="0" cellspacing="0" style="margin-bottom:10px;">
 <tr>
 <td width="55%" style="vertical-align:top; padding-right:6px;">
-    <table width="100%" cellpadding="2" cellspacing="0" border="0" class="thank-you" style="color:#2e7d32;">
-	    <tbody>
-	      <tr><td align="center">THANK YOU!</td></tr>
-	      <tr><td align="center">You will receive payment confirmation by email.<br>
-            Download Voucher from your account/manage trip<br><br>
-            We look forward to welcoming you to Mauritius<br>
-            and wish you a wonderful stay!</td>
-	        </tr>
-	      </tbody>
-	    </table>
+    &nbsp;
 </td>
 
 <td width="45%" style="vertical-align:top;">
