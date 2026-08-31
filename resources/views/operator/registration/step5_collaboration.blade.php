2981a2b69a81aab154044c6366799a267195f2fc
mehul-bcod <mehul@bcod.co.in>
2026-08-31 15:18:30 +0530

    admin container

--- resources/views/operator/registration/step5_collaboration.blade.php
+++ resources/views/operator/registration/step5_collaboration.blade.php
@@ -20,8 +20,8 @@
     <div id="sidebar" class="col-md-3 net-section">
         @include('operator.registration._sidebar', ['currentStep' => $currentStep, 'progress' => $progress ?? null])
     </div>
-    <div class="col-md-6 d-flex align-items-center justify-content-center" style="min-height: 90vh; margin-top: 30px;">
-        <div style="background: #fff; border-radius: 16px; box-shadow: 0 2px 16px rgba(0,0,0,0.07); padding: 32px 32px 24px 32px; width: 100%; max-width: 700px;">
+    <div class="col-md-6 d-flex align-items-center justify-content-center" style="">
+        <div class="container-middle">
             <h2 style="font-weight: bold; margin-bottom: 24px;">COLLABORATION AGREEMENT</h2>
             @if(session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
