c56b81e3a9a8167e1028f130802acc328bebdffe
mehul-bcod <mehul@bcod.co.in>
2026-09-24 11:52:17 +0530

    heading h3

--- resources/views/operator/drivers/_sidebar.blade.php
+++ resources/views/operator/drivers/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <!-- Drivers Sidebar -->
 <div style="">
-    <h6 style="color:#fff">Driver Management</h6>
+    <h3 style="color:#fff">Driver Management</h3>
     <div style="display:flex;flex-direction:column;gap:0px;">
         <a href="{{ route('operator.drivers.create') }}" style="padding:10px 12px;background:transparent;border-left:0px solid #19b5b5;border-radius:0px;text-decoration:none;font-size:13px;color:#fff;font-weight:600;">+ Add New Driver</a>
         <a href="{{ route('operator.drivers.index') }}" style="padding:10px 12px;background:transparent;border-left:0px solid #ccc;border-radius:0px;text-decoration:none;font-size:13px;color:#fff;font-weight:600;">Manage Drivers</a>
